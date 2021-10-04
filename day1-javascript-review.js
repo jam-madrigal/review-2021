fa248cd20a52eefdb339833d6d886f1b71553220
jam-madrigal <jam.madrigal@gmail.com>
2021-10-03 20:41:07 -0700

    break time.. added some explanations of equal to and less than/greater than operators

--- day1-javascript-review.js
+++ day1-javascript-review.js
@@ -53,6 +53,8 @@ console.log("After: ", JSON.stringify(testArray));
 
 // If statements: this is meant to demonstrate booleans, true or false, but all this function does to determine if it was true or false is see if there was a a parameter passed through for the value "wasItTrue". If ANYTHING exists in that as an argument, it will return "It was true", and if not, "It was false". In this application, true/the if statement essentially means yes, something exists as a parameter, and if it's null/blank, it's false. Try putting in different arguments into the test function to illustrate.
 
+// This can be elaborated on. For example, you can say if (wasItTrue === 5) etc. to ensure the "true" code after the if statement only runs if it is the same data type and value as specified. == (equality) Means it only checks for the same value, but not the same data type ie. a string vs an integer etc. There is also != for not equal, and !== for strict not equal. For best practice, use === (strict equality) as much as possible and refactor code to work this way if it doesn't already by changing logic to fit being more exacting. However, as usual, remember to have very DRY code (Do not Repeat Yourself)
+
 function test (wasItTrue) {
     if (wasItTrue) {
         return "It was true";
@@ -62,3 +64,7 @@ function test (wasItTrue) {
 
 console.log(test(true));
 console.log(test(false));
+
+// There is also greater than >, and less than <, operators. Expand these with >= or <= for greater than or equal to and less than or equal to.
+
+// End of day 1 review for now... had to help family a lot and constant interruptions. At least 20 minutes of coding a day keeps the unemployment away.. I think. Hopefully. All progress is good progress. We did more than that today anyway, considering the time spent thinking and explaining these concepts, and typing out the code while listening to lectures, rather than just looking at them and knowing I understand them.
