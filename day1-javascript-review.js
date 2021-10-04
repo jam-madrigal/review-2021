6595b9830f48538fbb0a6ddca76601721a14a188
jam-madrigal <jam.madrigal@gmail.com>
2021-10-03 19:30:06 -0700

    exercise 2 simulating a cue data structure

--- day1-javascript-review.js
+++ day1-javascript-review.js
@@ -33,3 +33,21 @@ console.log(showHat()); with the return uncommented
 
 // As demonstrated, the function still logs, in the console, the global variable, which in this case is a string.
 
+
+// In computer science, a cue is an abstract data structure. New items are added to the back of the cue, and old are removed from the front. Items are kept in order in this data structure. The following function will simulate it.
+
+// A function that can accept arguments in the form of an array and a new item to append to the array with push(), while also using shift() to remove an old item from the front
+
+function nextInLine(arr, item) {
+    testArray.push(item);
+    return arr.shift();
+
+}
+
+var testArray = ["dagger", "staff", "pointy hat", "tome", "robes"];
+// Demonstrating how the array looks on its own, logging the item to be removed (console logging the shift function does this) from the front of the array with shift(), and then logging the same array after using the next in line function to add another item to the end. Stringify added just as insurance: in this case it is not necessary because the array is already strings.
+
+console.log("Before: "  + JSON.stringify(testArray));
+console.log(nextInLine(testArray, "teardrop"));
+console.log("After: ", JSON.stringify(testArray));
+
